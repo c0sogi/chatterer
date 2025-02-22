5e27bc790dab10034fc1f3103877ddb72fded4f9
cosogi <dcas@naver.com>
2025-02-22 14:01:10 +0900

    Initialize llmcontext project with basic structure and dependencies

---
+++ llmcontext/__init__.py
@@ -0,0 +1,13 @@
+from .contexts import (
+    InstructorContext,
+    LangchainContext,
+    LLMContext,
+    OllamaContext,
+)
+
+__all__ = [
+    "InstructorContext",
+    "LangchainContext",
+    "LLMContext",
+    "OllamaContext",
+]
